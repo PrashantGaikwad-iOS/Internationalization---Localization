bb6a1c75d09fe95b3629059c2818d628d6476f75
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-08-15 22:33:03 +0530

    support for Spanish language

--- LocalizationSwift/ViewController.swift
+++ LocalizationSwift/ViewController.swift
@@ -23,3 +23,38 @@ class ViewController: UIViewController {
 
 }
 
+
+/*
+ 
+ Internationalization:
+ To generate Localizable.strings file -
+ In CMD
+ - cd /Users/prashantg/Desktop/Daily\ Tasks/August/LocalizationSwift/LocalizationSwift
+ - genstrings *.swift -o en.lproj
+ - Add the generated Localizable.strings file inside project(uncheck copy if needed)
+ 
+ In storyboard go to FileInspector and select your base language under Localization tab.
+ 
+ Localizing App Title:
+ - Add new InfoPlist.strings file inside your en.lproj (base language) file.
+ - Add your code in that file.
+ - eg :
+ "CFBundleDisplayName" = "Localización";
+ "NSHumanReadableCopyright" = "2016 Goldrush Computing Inc. All rights reserved.";
+ 
+ Localization:
+ To add multiple language support -
+    - Select your project in project navigator
+    - Select project name under PROJECT tab
+    - Under localizations tab, select your new language.
+
+ Change programatically text -
+ NSLocalizedString("Hi I am Prashant", comment: "Translate text: Hi I am Prashant")
+ 
+ Instead of changing app language from settings, try to change it from xcode:
+    - Select Edit scheme under Project name
+    - Select options tab and change application language from system language to your new language
+    - Run your project and see the updates
+ 
+ */
+
